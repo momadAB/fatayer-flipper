2865fc215bddd1d43798cecb481b449a2673cc09
momadAB <mabaqer99@gmail.com>
2024-10-19 12:05:17 +0300

    Added files for build

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import App from "./components/App.js";
-import { Agdasima } from "next/font/google";
-
-const agdasima = Agdasima({
-  weight: ["400", "700"],
-  subsets: ["latin"],
-});
 
 export default function Home() {
-  return <App className={agdasima.className} />;
+  return <App />;
 }
